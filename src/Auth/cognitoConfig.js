4699a85562fa98fa3b6b4c15f01ec04b63cf52f3
farida fadilah <faridafadilah42807@gmail.com>
2023-11-06 13:43:54 +0700

    initial

--- src/Auth/cognitoConfig.js
+++ src/Auth/cognitoConfig.js
@@ -1,4 +1,4 @@
 export const cognitoConfig = {
-  UserPoolId: "ap-southeast-1_QIeCpTfA8",
-  ClientId: "74mqhks26n2iam8fic54i046qe",
+  UserPoolId: "xxxxxxxxxxxxxxxxx",
+  ClientId: "xxxxxxxxxxxxxxxxx",
 }
